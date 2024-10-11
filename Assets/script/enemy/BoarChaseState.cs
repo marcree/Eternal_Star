40ec30d322e0a1f8f8d4dff2f12af0f27193d954
marcree <2976326707@qq.com>
2024-10-11 04:50:20 -0700

    update

---
+++ Assets/script/enemy/BoarChaseState.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoarChaseState : BaseState
+{
+    public override void OnEnter(Enemy enemy)
+    {
+        currentEnemy = enemy;
+        currentEnemy.currentSpeed = currentEnemy.chaseSpeed;
+        currentEnemy.anim.SetBool("run",true);
+        Debug.Log("run");
+    }
+    public override void LogicUpdate()
+    {
+        if(!currentEnemy.physicsCheck.isGround ||(currentEnemy.physicsCheck.touchLeftWall && currentEnemy.faceDir.x<0)|| (currentEnemy.physicsCheck.touchRightWall && currentEnemy.faceDir.x>0) )
+        {
+            currentEnemy.transform.localScale = new Vector3(currentEnemy.faceDir.x*1,1,1);
+        }
+    }
+    public override void PhysicsUpdate()
+    {
+
+    }
+    public override void OnExit()
+    {
+
+    }
+}
